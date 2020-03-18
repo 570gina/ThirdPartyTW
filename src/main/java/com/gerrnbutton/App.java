10b46504e17796503fb267785ff1f2196f9114ab
570gina <570gina@gmail.com>
2020-03-18 19:09:53 +0800

    fix bug (test)

--- src/main/java/com/gerrnbutton/App.java
+++ src/main/java/com/gerrnbutton/App.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class App 
@@ -15,8 +14,8 @@ public class App
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        PasswordEncoder encoder = new BCryptPasswordEncoder();
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         return encoder;
     }
 }
